ff34de451f10c98967767dc2efbf153d18fbf20e
Will Losey <wlosey35@gmail.com>
2016-05-12 11:44:50 -0700

    Add files via upload

---
+++ nodeschool/program2.js
@@ -0,0 +1,5 @@
+console.log(process.argv);
+var x = Number(process.argv[2])+Number(process.argv[3])+Number(process.argv[4])
+console.log ("THE SUM IS")
+console.log(x);
+console.log(process.argv[1])
---
+++ nodeschool/program2s.js
@@ -0,0 +1,6 @@
+sum= 0
+for (i=2; i<process.argv.length; i++){
+     console.log(i);
+     sum=sum + Number(process.argv[i]);
+}
+console.log(sum);
